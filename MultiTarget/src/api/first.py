1f7f06f48bdb4c0da69a7fcbb811bc971e047e1c
seyfettin.yumlu <seyfettin.yumlu@vizyoneks.com.tr>
2018-12-16 23:28:26 +0300

    3

--- MultiTarget/src/api/first.py
+++ MultiTarget/src/api/first.py
@@ -1,21 +1,35 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
+from flask import Flask
+
+app = Flask(__name__)
+
 es = Elasticsearch()
 
-doc = {
-    'author': 'kimchy',
-    'text': 'Elasticsearch: cool. bonsai cool.',
-    'timestamp': datetime.now(),
-}
-res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
-print(res['result'])
+@app.route("/")
+def Index():
+    TestElastic()
+    return "Hi there, this is a web api"
 
-res = es.get(index="test-index", doc_type='tweet', id=1)
-print(res['_source'])
+def TestElastic():
+    doc = {
+        'author': 'seyfi',
+        'text': 'Elasticsearch: cool. bonsai cool.',
+        'timestamp': datetime.now(),
+    }
+    res = es.index(index="test-index", doc_type='tweet', id=2, body=doc)
+    print("1 -> "+res['result'])
 
-es.indices.refresh(index="test-index")
+    res = es.get(index="test-index", doc_type='tweet', id=2)
+    print(res['_source'])
 
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
-print("Got %d Hits:" % res['hits']['total'])
-for hit in res['hits']['hits']:
-    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+    es.indices.refresh(index="test-index")
+
+    res = es.search(index="test-index", body={"query": {"match_all": {}}})
+    print("Got %d Hits:" % res['hits']['total'])
+    for hit in res['hits']['hits']:
+        print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+
+if __name__ == "__main__":
+    app.run(port=5001)
+    
